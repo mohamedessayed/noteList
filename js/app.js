2aa918e4d774b81ac976a78c51c833f0e867262e
mohamedessayed <mlovely9316@gmail.com>
2024-12-05 19:09:42 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,96 @@
+var notesList = [];
+var selectedNote = -1;
+
+var titleInput = document.getElementById('exampleInputTitle');
+var contentInput = document.getElementById('exampleFormControlTextareaConetnt');
+var tableView = document.getElementById('tableView');
+var mainBtnApp = document.getElementById('mainBtnApp');
+
+function btnApp() {
+    if (mainBtnApp.innerText === 'save') {
+        createNewNote();
+    }else{
+        updateNoteInList();
+    }
+}
+
+function createNewNote(){
+    // var titleValue = titleInput.value;
+    // var contentValue = contentInput.value;
+    //generate object
+    var noteObject = {
+        title:titleInput.value,
+        note:contentInput.value,
+    }
+    //push object into array
+    notesList.push(noteObject);
+    
+    //invoke read function
+    readNotesList();
+
+    //clear form
+    clearForm();
+}
+
+function readNotesList(){
+    var htmlCollection =``;
+    var index = 0;
+    for (var note of notesList) {
+        htmlCollection += `<tr>
+            <th scope="row">${index}</th>
+            <td>${note.title}</td>
+            <td>
+                <button onclick="deleteNoteFromList(${index})" class="btn btn-danger" type="button">Delete</button>
+                <button onclick="editNoteFromList(${index})" class="btn btn-warning" type="button">Edit</button>
+            </td>
+          </tr>`;
+          index++;
+    }
+    tableView.innerHTML = htmlCollection;
+}
+
+function deleteNoteFromList(index){
+    notesList.splice(index,1);
+    readNotesList();
+}
+
+function clearForm() {
+    
+    titleInput.value = '';
+    contentInput.value = '';
+}
+
+function editNoteFromList(index){
+    var selectedNoteObject = notesList[index];
+    
+    titleInput.value = selectedNoteObject.title;
+    contentInput.value = selectedNoteObject.note;
+    
+    //change form button text to 'update'
+
+    mainBtnApp.innerText = 'update';
+
+    //detection for which index will be update by assign the selected index to global variable
+
+    selectedNote = index;
+}
+
+function updateNoteInList() {
+    var noteObject = {
+        title:titleInput.value,
+        note:contentInput.value,
+    }
+
+    // notesList[selectedNote] = noteObject;
+
+    notesList.splice(selectedNote,1,noteObject);
+
+    readNotesList();
+
+    clearForm();
+
+    //change form button text to 'save'
+
+    mainBtnApp.innerText = 'save';
+    
+}
---
+++ js/index.js
@@ -0,0 +1,218 @@
+/*
+
+coding style
+----------
+1- sequentional
+2- condtioning (if, switch)
+3- iteration "Loop"
+
+Basics
+------------
+html code ==> document
+web browser ==> window
+
+
+variables متغير
+
+
+x  y   op
+----------
+2  3   +
+3  2   *
+
+
+Data types
+-----------
+1-number (integer ex: 1 , 100 , 200 , -50 / float: 1.20 , -50.6)
+2-string
+3-boolean
+4-undefined
+5-object (array [], null, object {})
+
+user inputs
+-----------
+1- web browser ==> prompt
+2- HTML code ==> form
+
+Operators
+---------
+
+1- Math operator
+
++ addition
+- subtract
+* multiplication
+/ division
+% moodulation
+
+
+2- string operator
+
++ concat
+
+3- comparison operator (True / Fasle)
+
+> grater than
+< less than
+>= grater than or equal
+<= less than or equal
+== is equal
+!= is not equal
+
+=== identical equal
+!== Not identical equal
+
+
+4- Logical operators
+
+&& AND
+
+C1 && C2  R
+T     F   F
+F     T   F
+T     T   T
+F     F   F
+
+-------------
+
+|| OR
+
+C1 || C2  R
+T     F   T
+F     T   T
+T     T   T
+F     F   F
+
+---------------
+
+!  NOT
+
+C1  R
+!T  F
+!F  T
+------------
+
+condtioning 
+
+1- if(condtion){}
+2- if(condtion){}else{}
+3- if(condtion){}else if(condtion){}else if(condtion){}else{}
+4- switch
+
+Loop
+1- for(counter; condtion; counter_update){}
+2- while(condtion){  counter_update}
+3- do{ counter_update}while(condtion);
+4- for(element of array){}
+5- array.forEach(function(element){})
+
+loop controllers
+
+1- break;
+2- continue;
+
+-- Divided my code --
+1-ease of mantine
+2-resue
+
+Declrtion function
+-------
+syntax:
+
+function __name__ (){ 
+
+//logic
+
+}
+__name__()  // function call / invoke
+
+Scope
+-------
+
+Global
+Local (Function only)
+
+expression function
+-------
+var __functionName__ = function(__Inputs__){
+//logic
+}
+
+self invoke
+----------
+(function(){})();
+
+anyonymous function
+----------------
+function(){}
+
+Array (List) built in functions & properties
+
+1-Properties
+-length
+
+2-functions
+-- add functions
+** push()
+** unshift()
+-- Remove functions
+** pop()
+** shift()
+** splice(startIndex, deleteCount)
+-- cut slice
+**slice(start, end)
+--search
+**indexOf(searchItem)
+**includes(searchItem)
+--reverse array
+**reverse()
+--replace
+** splice(startIndex, deleteCount,newValue)
+------------------
+
+object
+
+var table = {
+    height : '90cm',
+    price: 30,
+    width:'120cm',
+    depth:'30cm',
+    material : 'Wood',
+    images:['table.png','table2.png','table3.png'],
+    sellers:[{
+        name:'ahmed',
+        country:'KSA',
+        store:'Dalel'
+    },
+    {
+        name:'mohamed',
+        country:'UEA',
+        store:'Dalel'
+    },{
+        name:'Ali',
+        country:'EGP',
+        store:'Dalel'
+    }]
+}
+
+console.log(typeof(table));
+console.log(table.height);
+
+table.height = '80cm';
+table['height'] = '70cm';
+
+console.log(table);
+
+console.log(table.images[1]);
+console.log(table.sellers[1].country);
+
+--------------------------------------
+
+*/
+
+
+
+
+
+
+
